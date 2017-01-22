c28b1b3c24eca3aee4625440ed334770f2ba6cd8
epetrix <edendawnpetri@gmail.com>
2017-01-22 00:26:35 -0500

    delete

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,12 +1,12 @@
-var provider = new firebase.auth.GoogleAuthProvider();
 
 function toLogin() {
+  console.log("lol sorry");
+	var provider = new firebase.auth.GoogleAuthProvider();
 	firebase.auth().signInWithPopup(provider).then(function(result) {
   	// This gives you a Google Access Token. You can use it to access the Google API.
   	var token = result.credential.accessToken;
   	// The signed-in user info.
   	var user = result.user;
-	console.log(user);
   	// ...
 	}).catch(function(error) {
   // Handle Errors here.
@@ -18,3 +18,13 @@ function toLogin() {
   	var credential = error.credential;
 	});
 }
+
+Pinder.prototype.onAuthStateChanged = function(user) {
+  console.log(user);
+  if (user) { // User is signed in!
+    // Get profile pic and user's name from the Firebase user object.
+    var profilePicUrl = user.photoURL;
+    var userName = user.displayName;
+	}
+}
+
