10cb00e6e3c6ba4eb3a81c8f48fb5dac89549639
epetrix <edendawnpetri@gmail.com>
2017-01-22 02:34:10 -0500

    please?

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,38 +1,27 @@
-var config = {
-  apiKey: "AIzaSyAzgDLUbaN731nHMPIGd96f7QSY5unzc5g",
-  authDomain: "she-innovates.firebaseapp.com",
-  databaseURL: "https://she-innovates.firebaseio.com",
-  storageBucket: "she-innovates.appspot.com",
-  messagingSenderId: "751801012241"
-};
 
-firebase.initializeApp(config);
+function signIn(){
 
-function toLogin() {
-    console.log("lol sorry");
-	var provider = new firebase.auth.GoogleAuthProvider();
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-  	// This gives you a Google Access Token. You can use it to access the Google API.
-  	var token = result.credential.accessToken;
-  	// The signed-in user info.
-  	var user = result.user;
-  	// ...
-	}).catch(function(error) {
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   // Handle Errors here.
-  	var errorCode = error.code;
-  	var errorMessage = error.message;
-  // The email of the user's account used.
-  	var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  	var credential = error.credential;
-	});
-}
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  
+});
 
-firebase.auth().onAuthStateChanged = function(user) {
-  console.log(user);
-  if (user) { // User is signed in!
-    // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = user.photoURL;
-    var userName = user.displayName;
-	}
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+    if (errorCode === 'auth/wrong-password') {
+    alert('Wrong password.');
+  } else {
+    alert(errorMessage);
+  }
+});
+
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}, function(error) {
+  // An error happened.
+});
 }
