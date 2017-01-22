f9b750d58546a156fb0c2e4c177caeeecc6ca921
Sharon Gao <isotera@gmail.com>
2017-01-21 23:17:19 -0500

    Add files via upload

---
+++ public/js/boots.js
@@ -0,0 +1,39 @@
+var Questions = { "question" : [
+  {
+    "question"  : "loafer",
+		"itemName"	: "Sixty Seven Grommet Patent Leather Loafers",
+     "choice0"  : "$0",
+     "choice1"  : "$10",
+     "choice2"  : "$20",
+     "choice3"  : "$30",
+     "choice4"  : "$40",   
+	 	"choice5"  : "$50",                  
+     "correct"  : 40.00,
+	  "description" : "Head into the cooler season in the right look with the Grommet Patent Leather Loafers. Spanish brand Sixty Seven adds patent leather to these loafers for a high-shine look, while a lug sole and metal grommets round out the details. Rock them with plaid and denim for preppy-chic style."
+  },
+  {
+    "question"  : "boot",
+		"itemName"	: "Faux Suede Knee-High Boots",
+     "choice0"  : "$0",
+     "choice1"  : "$10",
+     "choice2"  : "$20",
+     "choice3"  : "$30",
+     "choice4"  : "$40",   
+	 	"choice5"  : "$50",                  
+     "correct"  : 30.00,
+	  "description" : "Qupid adds fun flair to a classic boot with the Vinci Faux Suede Knee-High Boots. Available in two colors, these knee-high boots are crafted from smooth faux suede with a drawstring closure, pointy almond toe and low heel."
+  },
+  {
+    "question"  : "combat",
+		"itemName"	: "Faux Leather Combat Boots",
+     "choice0"  : "$0",
+     "choice1"  : "$10",
+     "choice2"  : "$20",
+     "choice3"  : "$30",
+     "choice4"  : "$40",   
+	 	"choice5"  : "$50",                  
+     "correct"  : 20.00,
+	  "description" : "Stay ahead of the rest in this timeless combat boot by NYLA Shoes. Crafted from sleek faux leather, these combat boots draw on military inspiration with their lace-up design, rounded toe cap, and lug sole for traction and support."
+  }
+  
+]};
